c5900bf33d6adfccc05a37b414645c1d435d2ce8
xasiyevsamir <samirakh@code.edu.az>
2023-05-29 13:58:05 +0400

    yeni

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int data;
+            while (true)
+            {
+                try
+                {
+                    data = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception )
+                {
+                    Console.WriteLine("duzgun daxil edin !");
+
+                }
+            }
+        }
+    }
+}
