4e8177fd7667c8bda2f46b8bca13e80018f86fca
Xasiyev_Samir <125192688+xasiyevsamir@users.noreply.github.com>
2023-06-01 22:49:38 +0400

    Update Program.cs

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -38,7 +38,8 @@ namespace ConsoleApp5
             Console.WriteLine("samir");
             Console.WriteLine("main branch deyisirik");
             int da=124;
-            int k=22;
+            int k=22;;
+            
         }
     }
 }
